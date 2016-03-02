af2bc1ada0dcf3a2cc55cdf62158df6b59bb08f7
Daniel Dyba <daniel.dyba@gmail.com>
2016-03-01 16:58:06 -0800

    Fixes tests; migrates to expect syntax

--- spec/action_config_spec.rb
+++ spec/action_config_spec.rb
@@ -5,23 +5,28 @@ module ActionParamCaching
     context "in order to track the cache configuration of an action" do
       it "is initialized with valid params, an action, and a prefix filter" do
         cache_config = ActionParamCaching::ActionConfig.new("some_controller", :index,[:id, :name, :amount], '_')
-        cache_config.action.should == :index
-        cache_config.valid_params.should == [:id, :name, :amount, :controller, :action, :format]
-        cache_config.filter_starting_with.should == '_'
+
+        expect(cache_config.action).to eq :index
+        expect(cache_config.valid_params).to eq [:id, :name, :amount, :controller, :action, :format]
+        expect(cache_config.filter_starting_with).to eq '_'
       end
 
       it "sets the correct default values for valid params and prefix filters" do
         cache_config = ActionParamCaching::ActionConfig.new("some_controller", :index)
-        cache_config.action.should == :index
-        cache_config.valid_params.should == nil
-        cache_config.filter_starting_with.should == '_'
+
+        expect(cache_config.action).to eq :index
+        expect(cache_config.valid_params).to eq nil
+        expect(cache_config.filter_starting_with).to eq '_'
       end
 
       it "tracks if a cache has been configured" do
         cache_config = ActionParamCaching::ActionConfig.new("some_controller", :index)
-        cache_config.did_config_cache.should be_false
+
+        expect(cache_config.did_config_cache).to eq false
+
         cache_config.did_config_cache = true
-        cache_config.did_config_cache.should be_true
+
+        expect(cache_config.did_config_cache).to eq true
       end
     end
 
@@ -29,13 +34,14 @@ module ActionParamCaching
       it "has an if condition when you specify valid params" do
         cache_config = ActionParamCaching::ActionConfig.new("some_controller", :index,[:id, :name, :amount], '_')
         cache_args = cache_config.cache_args
-        cache_args[:if].should be
+        expect(cache_args[:if]).not_to eq nil
       end
 
       it "does not have an if condition if you don't specify valid params" do
         cache_config = ActionParamCaching::ActionConfig.new("some_controller",:index)
         cache_args = cache_config.cache_args
-        cache_args[:if].should be_nil
+
+        expect(cache_args[:if]).to eq nil
       end
 
       it "has a the correct cache path when stripping params with a prefix" do
@@ -44,7 +50,7 @@ module ActionParamCaching
         request.stubs(:params).returns( {:_remove_me => true, :controller => "some_controller", :action => :index, :format => 'json'} )
         cache_args = cache_config.cache_args
         proc = cache_args[:cache_path]
-        proc.call(request).should == {:controller => "some_controller", :action => :index, :format => 'json'}.to_query
+        expect(proc.call(request)).to eq({:controller => "some_controller", :action => :index, :format => 'json'}.to_query)
       end
 
       it "has a the correct cache path under normal conditions" do
@@ -53,13 +59,13 @@ module ActionParamCaching
         request = mock
         request.stubs(:params).returns( {:_remove_me => false, :controller => "some_controller", :action => :index, :format => 'json'} )
         proc = cache_args[:cache_path]
-        proc.call(request).should == {:controller => "some_controller", :action => :index, :format => 'json'}.to_query
+        expect(proc.call(request)).to eq({:controller => "some_controller", :action => :index, :format => 'json'}.to_query)
       end
 
       it "does not have a if argument if the valid params are empty" do
         cache_config = ActionParamCaching::ActionConfig.new("some_controller", :index)
         cache_args = cache_config.cache_args
-        cache_args[:if].should be_nil
+        expect(cache_args[:if]).to be_nil
       end
 
       it "does have an if argument that returns true if the params are a subset of the valid params" do
@@ -68,7 +74,7 @@ module ActionParamCaching
         request = mock
         request.stubs(:params).returns( {:id => 23, :name => "test", :controller => "some_controller", :action => :index, :format => 'json'} )
         proc = cache_args[:if]
-        proc.call(request).should be_true
+        expect(proc.call(request)).to eq true
       end
 
       it "does have an if argument that returns false if the params are not a subset of the valid params" do
@@ -77,7 +83,7 @@ module ActionParamCaching
         request = mock
         request.stubs(:params).returns( {:id => 23, :name => "test", :not => "valid", :controller => "some_controller", :action => :index, :format => 'json'} )
         proc = cache_args[:if]
-        proc.call(request).should be_false
+        expect(proc.call(request)).to eq false
       end
       
       it "adds expires in parameter to action arguments" do
@@ -85,7 +91,8 @@ module ActionParamCaching
         cache_args = cache_config.cache_args
         request = mock
         request.stubs(:params).returns( {:id => 23, :name => "test", :not => "valid", :controller => "some_controller", :action => :index, :format => 'json', :expires_in => 24.hours} )
-        cache_args[:expires_in].should eq(24.hours)
+
+        expect(cache_args[:expires_in]).to eq 24.hours
       end
     end
   end
--- spec/rails/action_controller_spec.rb
+++ spec/rails/action_controller_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 class TestController
   extend ActionParamCaching::Rails::ActionController
+
   def self.caches_action(action, args)
   end
+
   def self.controller_path
     "some_controller"
   end
@@ -13,26 +15,30 @@ module ActionParamCaching
   module Rails
     describe ActionController do
       it "provides param configuration for action caching" do
-        TestController.respond_to?(:action_cache_configs).should be_true
+        expect(TestController).to respond_to(:action_cache_configs)
       end
 
       it "provides a means to configure caching for mutiple actions with one statement" do
         TestController.cache_with_params :on => [:test1]
-        TestController.action_cache_configs[:test1].should be
+
+        expect(TestController.action_cache_configs[:test1].action).to eq :test1
       end
 
       it "provided a means to set a set or subset or params to cache on" do
         TestController.cache_with_params :on => [:test2], :with_set_or_subset => [:param1, :param2]
-        TestController.action_cache_configs[:test2].valid_params.should == [:param1, :param2, :controller, :action, :format]
+
+        expect(TestController.action_cache_configs[:test2].valid_params).to eq [:param1, :param2, :controller, :action, :format]
       end
 
       it "provides a means to filter params that have a prefix" do
         TestController.cache_with_params :on => [:test3], :filter_starting_with => '_'
-        TestController.action_cache_configs[:test3].filter_starting_with.should == '_'
+
+        expect(TestController.action_cache_configs[:test3].filter_starting_with).to eq '_'
       end
 
       it "configures the cache actions using the specified params" do
         TestController.expects(:caches_action)
+
         TestController.cache_with_params :on => [:test4], :filter_starting_with => '_', :with_set_or_subset => [:param1, :param2]
       end
     end
